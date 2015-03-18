687431ae0b6f55a0095a2aafe6fe5dd5d78c21be
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-18 16:56:37 +0000

    Update config #1

--- config/admin.php
+++ config/admin.php
@@ -2,8 +2,13 @@
 
 return [
 
+	// Enter your plugin license key here
 	'license_key' => '',
 
+	// Choose which type of editor you would like: simple/advanced
+	'editor' => 'simple',
+
+	// Add generated users to this array (visit /admin/create-user)
 	'users' => [
 
 	]
